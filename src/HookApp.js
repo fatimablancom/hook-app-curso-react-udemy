d04abd223c0a52466e4796207ad85a5a467eccad
Fatima Blanco <fatimablancomunoz@gmail.com>
2022-04-25 16:52:07 +0200

    feat: new hook app, bootstrap link

---
+++ src/HookApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CounterApp } from './components/01-useState/CounterApp'
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
+
+export const HookApp = () => {
+  return (
+    <>
+        <CounterApp />
+        <hr/>
+        <CounterWithCustomHook />
+    </>
+
+  )
+}
