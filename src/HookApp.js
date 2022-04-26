97fe930be7969fc03b8c5e0d35a4162f1f16e4ab
Fatima Blanco <fatimablancomunoz@gmail.com>
2022-04-26 17:11:00 +0200

    feat: new component FormWithCustomHook (useForm)

--- src/HookApp.js
+++ src/HookApp.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { SimpleForm } from './components/01-useState/02-useEffect/SimpleForm'
 import { CounterApp } from './components/01-useState/CounterApp'
 import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook'
+import { FormWithCustomHook } from './components/01-useState/02-useEffect/FormWithCustomHook'
 
 export const HookApp = () => {
   return (
@@ -12,6 +13,7 @@ export const HookApp = () => {
         <hr/>
         <SimpleForm />
         <hr/>
+        <FormWithCustomHook/>
     </>
 
   )
---
+++ src/components/01-useState/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import './effects.css';
+import { useForm } from '../../../hooks/useForm';
+
+export const FormWithCustomHook = () => {
+
+    const [formValues, handleInputChange] = useForm ({
+        name: '',
+        email:'',
+        password: ''
+    })
+
+    const {name, email, password} = formValues;
+
+  useEffect(() => {
+    console.log('email modificado')
+  }, [email]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues)
+
+  }
+    
+  return (
+      <form onSubmit={handleSubmit}>
+        <h1>FormWithCustomHook</h1>
+
+        <div className='form-group'>
+            <input 
+                type="text"
+                name="name"
+                className='form-control'
+                placeholder='Tu nombre'
+                autoComplete='off'
+                value={name}
+                onChange={handleInputChange}
+            />
+        </div>
+
+        <div className='form-group'>
+            <input 
+                type="text"
+                name="email"
+                className='form-control'
+                placeholder='Email'
+                autoComplete='off'
+                value={email}
+                onChange={handleInputChange}
+            />
+        </div>
+
+        <div className='form-group'>
+            <input 
+                type="password"
+                name="password"
+                className='form-control'
+                placeholder='******'
+                autoComplete='off'
+                value={password}
+                onChange={handleInputChange}
+            />
+        </div>
+
+        <button typeof='submit' className='btn btn-primary'>Guardar</button>
+      </form>
+  )
+}
+
